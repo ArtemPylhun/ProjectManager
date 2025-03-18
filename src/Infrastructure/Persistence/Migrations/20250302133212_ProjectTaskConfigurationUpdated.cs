2cf82def768d7a0ece40e4854f3a887ffd215f9b
ArtemPylhun <artem.pylhun@oa.edu.ua>
2025-03-18 17:52:43 +0200

    feat: migrations added

---
+++ src/Infrastructure/Persistence/Migrations/20250302133212_ProjectTaskConfigurationUpdated.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectTaskConfigurationUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_project_tasks_users_creator_id",
+                table: "project_tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_project_tasks_users_creator_id",
+                table: "project_tasks",
+                column: "creator_id",
+                principalTable: "AspNetUsers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_project_tasks_users_creator_id",
+                table: "project_tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_project_tasks_users_creator_id",
+                table: "project_tasks",
+                column: "creator_id",
+                principalTable: "AspNetUsers",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
